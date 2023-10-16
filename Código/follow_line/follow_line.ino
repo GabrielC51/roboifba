10cba003d5838e5921ad44018a0f84695281a74d
Gabriel <gabrielcmonteiro3@gmail.com>
2023-10-16 17:22:38 -0300

    seguindo linha com carcaça antiga

--- Código/follow_line/follow_line.ino
+++ Código/follow_line/follow_line.ino
@@ -1,124 +1,75 @@
-#include <PID_v1.h> 
+int ir[2];
+int branco = 500;
+int motorEsq1 = 5, motorEsq2 = 6, motorDir1 = 9, motorDir2 = 10;  //valor base do motor
 
-//variaveis do sensor de cor
-int verm = 2, verde = 4, azul = 7;
-int RGB[3][3]; //[n° do sensor][R, G e B]
-int IResq = A0, IRdir = A1, corDir = A6, corEsq = A7;
-const int fita_verde[2][3] = {//[0 = mínimo | 1 = máximo][R, G e B](ajustar valores com teste e se deve mudar o padrão de 1 byte)
-  {0, 900, 0}, 
-  {300, 1023, 300}
-};
-const int branco[3] = {900, 900, 900}; //verificar nescessidade disso
-
-//variaveis do PID
-double sp, in, out;
-float kp = 0.5, ki = 0, kd = 0; //valores ainda a serem realmente definidos
-PID PID(&in, &out, &sp, kp, ki, kd, DIRECT);
-
-//variaveis do motor
-int vb = 205, motorEsq1 = 5, motorEsq2 = 6, motorDir1 = 9, motorDir2 = 10;  //valor base do motor
-void lerlinha(){
-  int t; //tempo para ldr "acostumar" com brilho
-  digitalWrite(verm, LOW); //força desligamento de todos os leds
-  digitalWrite(verde, LOW);
-  digitalWrite(azul, LOW);
-  delay(10); //pequeno dalay
-
-  //lê vermelho
-  digitalWrite(verm, HIGH);
-  delay(t);
-  RGB[0][0] = analogRead(corDir);
-  RGB[1][0] = analogRead(corEsq);
-  RGB[2][0] = analogRead(corGarra);
-  digitalWrite(verm, LOW);
-
-  //lê verde
-  digitalWrite(verde, HIGH);
-  delay(t);
-  RGB[0][1] = analogRead(corDir);
-  RGB[1][1] = analogRead(corEsq);
-  RGB[2][1] = analogRead(corGarra);
-  digitalWrite(verde, LOW);
-
-  //lê azul
-  digitalWrite(azul, HIGH);
-  delay(t);
-  RGB[0][2] = analogRead(corDir);
-  RGB[1][2] = analogRead(corEsq);
-  RGB[2][2] = analogRead(corGarra);
-  digitalWrite(azul, LOW);
+void frente(byte velo){
+  analogWrite(motorEsq1, velo);
+  analogWrite(motorEsq2, 0);
+  analogWrite(motorDir1, velo);
+  analogWrite(motorDir2, 0);
 }
 
-String comparaCor(byte porta){
-  //retorna cor na porta escolhida, por enquanto apenas preto, verde e branco.
-  String retorno;
-  if(RGB[porta][0] <= fita_verde[1][0] && RGB[porta][2] <= fita_verde[1][2] && RGB[porta][1] >= fita_verde[0][1] && RGB[porta][1] <= fita_verde[1][1]){
-    retorno = "verde";
-  }else if(RGB[porta][0] >= branco[0] && RGB[porta][1] >= branco[1] && RGB[porta][2] >= branco[2]){
-    retorno = "branco";
-  }else{
-    retorno = "preto";
+void tras(byte velo){
+  analogWrite(motorDir1, 0);
+  analogWrite(motorDir2, velo);
+  analogWrite(motorEsq1, 0);
+  analogWrite(motorEsq2, velo);
+}
+void curvaEsq(byte velo, byte dif){
+  if(velo - dif < 0){
+    parar();
+    return;
   }
-  return retorno;
+  analogWrite(motorEsq1, velo);
+  analogWrite(motorEsq2, 0);
+  analogWrite(motorDir1, velo - dif);
+  analogWrite(motorDir2, 0);
+}
+void curvaDir(byte velo, byte dif){
+  if(velo - dif < 0){
+    parar();
+    return;
+  }
+  analogWrite(motorEsq1, velo - dif);
+  analogWrite(motorEsq2, 0);
+  analogWrite(motorDir1, velo);
+  analogWrite(motorDir2, 0);
+}
+
+void parar(){
+  analogWrite(motorDir1, 0);
+  analogWrite(motorDir2, 0);
+  analogWrite(motorEsq1, 0);
+  analogWrite(motorEsq2, 0);
 }
 
 void setup() {
+  //put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(corDir, INPUT);
-  pinMode(corEsq, INPUT);
-  pinMode(corGarra, INPUT);
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-
-  pinMode(verm, OUTPUT);
-  pinMode(verde, OUTPUT);
-  pinMode(azul, OUTPUT);
   pinMode(motorEsq1, OUTPUT);
   pinMode(motorEsq2, OUTPUT);
   pinMode(motorDir1, OUTPUT);
   pinMode(motorDir2, OUTPUT);
-
-  sp = 511; //valor de "meio" da entrada analogica
-  SetOutputLimits(-50, 50) //muda valor da saídas
-  PID.SetMode(AUTOMATIC);  //seta controlador pid como automático
 }
 
 void loop() {
-  bool arraylinha[4]; //armazena informações sobre a linha sendo true para branco e false para preto
-  int SP_base = 50; //nosso setpoint, mas em porcentagem
-  int in_percent; //guarda a porcentagem da entrada, calculo abaixo
+  // put your main code here, to run repeatedly:
+  ir[0] = analogRead(A0);
+  ir[1] = analogRead(A1);
+  bool ir_bool[2];
+  ir_bool[0] = ir[0] >= 600;
+  ir_bool[1] = ir[1] >= 600;
   
-  lerlinha(); //faz leitura da linha, podemos unir com a função comparaCor(), para ver se isso facilita o trabalho
-  //aqui ele verifica se os dois sensores retornam valores equivalentes a verde
-  if(comparaCor(0) == "verde"){
-    //virar para o lado do sensor 0
-  }else if(comparaCor(1) == "verde"){
-    //virar para o lado do sensor 1
+  if(ir_bool[0] && ir_bool[1]){
+    frente(60);
+  }else if(ir_bool[0]){
+    curvaDir(60, 50);
+  }else if(ir_bool[1]){
+    curvaEsq(60, 50);
   }else{
-    arraylinha[0] = (digitalRead(12) >= BrancoIR) ? true : false;
-    arraylinha[1] = (comparaCor(0) == "branco") ? true : false;
-    arraylinha[2] = (comparaCor(1) == "branco") ? true: false;
-    arraylinha[3] = (digitalRead(13) >= BrancoIR) ? true : false;
-    //verifica o valor armazenado de cada sensor e faz uma operação baseado na cor detectada, variando de 0% a 100%
-    if(!arraylinha[0]){
-      in_percent = SP_base - 50;
-    }else if(!arraylinha[1]){
-      in_percent = SP_base - 25;
-    }else if(!arraylinha[2]){
-      in_percent = SP_base + 25;
-    }else if(!arraylinha[3]){
-      in_percent = SP_base + 50;
-    }
-    in = map(in_percent, 0, 100, 0, 1023); //mapea do valor da porcentagem para um valor equivalente na escala aceita pela lib do pid
-    PID.Compute(); //faz o pid, depois printa para nossos testes, falta ver a melhor de passar isso para os motores
-    digitalWrite(motorEsq1, vb + out);
-    digitalWrite(motorEsq2, 0);
-    digitalWrite(motorDir1, vb - out);
-    digitalWrite(motorDir2, 0);
-
-    Serial.print("in: ");
-    Serial.print(in);
-    Serial.print(" out: ");
-    Serial.println(out);
+    frente(60);
   }
+  delay(10);
 }
--- Código/teste_sensor_IR/teste_sensor_IR.ino
+++ Código/teste_sensor_IR/teste_sensor_IR.ino
@@ -1,23 +1,11 @@
-#include <PID_v1.h>
-
 int ir[2];
-int branco = 500, in_perc;
-double in, out, sp = 500; //valor do PID
-float kp = 0.5, ki = 0, kd = 0; //valores ainda a serem realmente definidos
-int vb = 205, motorEsq1 = 5, motorEsq2 = 6, motorDir1 = 9, motorDir2 = 10;  //valor base do motor
-PID pid(&in, &out, &sp, kp, ki, kd, DIRECT);
-
+int branco = 600;
 
 void setup() {
   //put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-  pinMode(motorEsq1, OUTPUT);
-  pinMode(motorEsq2, OUTPUT);
-  pinMode(motorDir1, OUTPUT);
-  pinMode(motorDir2, OUTPUT);
-  pid.SetMode(AUTOMATIC);  //seta controlador pid como automático
 }
 
 void loop() {
@@ -25,26 +13,10 @@ void loop() {
   ir[0] = analogRead(A0);
   ir[1] = analogRead(A1);
   bool ir_bool[0];
-  ir_bool[0] = (ir[0] >= 600) ? false:true;
-  ir_bool[1] = (ir[1] >= 600) ? false:true;
+  ir_bool[0] = ir[0] >= 600;
+  ir_bool[1] = ir[1] >= 600;
   Serial.print(ir[0]);
   Serial.print("|");
   Serial.println(ir[1]);
-  if(ir_bool[0]){
-    analogWrite(motorDir1, 50);
-    analogWrite(motorEsq1, 0);
-    analogWrite(motorDir2, 0);
-    analogWrite(motorEsq2, 50);
-  }else if(ir_bool[1]){
-    analogWrite(motorDir1, 0);
-    analogWrite(motorEsq1, 50);
-    analogWrite(motorDir2, 50);
-    analogWrite(motorEsq2, 0);
-  }else{
-    analogWrite(motorDir1, 50);
-    analogWrite(motorEsq1, 50);
-    analogWrite(motorDir2, 0);
-    analogWrite(motorEsq2, 0);
-  }
-  delay(600);
+  delay(500);
 }
